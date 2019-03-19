c4d3c791d42daf934c12e92691b2da4d9220f26f
aurelienantonio <aurelien.antonio@gmail.com>
2019-03-19 12:52:25 -0500

    First Commit ( function program)

---
+++ MisteryNumber/Player.swift
@@ -0,0 +1,48 @@
+//
+//  Player.swift
+//  MisteryNumber
+//
+//  Created by Aurélien Antonio on 19/03/2019.
+//  Copyright © 2019 Aurélien Antonio. All rights reserved.
+//
+
+import Foundation
+
+class Player { // Je suis la Class Player
+    
+    let name : String // variable d'instance : elle de l'instance; chaque instance à une valeur différente
+    static var intents : [Int] = [] // variable de classe; la valeure est commune à toutes les instances
+    
+    init(name: String){
+        self.name = name
+    }
+    
+    func answer(misteryNumber: Int) -> Bool{
+        print("\(name) please choose a number")
+        let answer = chooseNumber()
+        if Player.intents.contains(answer){
+            print("This number as already been selected and is not the correct one, please select an other number. You can play again directly")
+            return self.answer(misteryNumber: misteryNumber)
+        }
+        Player.intents.append(answer)
+        print(Player.intents)
+        if (misteryNumber == answer){
+            print("\(name) You Win")
+            return true
+        }else{
+            print("this is not the misteryNumber, please try again")
+            return false
+        }
+    }
+    func chooseNumber() -> Int{
+
+        if let intent = readLine() {
+            if let number = Int(intent){
+                return number
+            }
+        }
+        print("Entrez un nombre entier !")
+        return chooseNumber()
+    }
+    
+}
--- MisteryNumber/main.swift
+++ MisteryNumber/main.swift
@@ -8,5 +8,23 @@
 
 import Foundation
 
-print("Hello, World!")
+print("""
+    Welcome to our Mistery Number Game:
+    You must discover what is the mistery number in the minimum attempts
+    You can't use a number already used !
+""")
+
+let misteryNumber = Int.random(in: 1...100)
+print(misteryNumber)
+var found = false
+
+let p1 = Player(name: "Tonio") // Je suis une Instance de player -> Une instance de class c'est l'appel à la classe
+let p2 = Player(name: "Massi")
+
+repeat {
+   found = p1.answer(misteryNumber: misteryNumber)
+    if(!found){
+        found = p2.answer(misteryNumber: misteryNumber)
+    }
+} while !found
 
